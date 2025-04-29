c8bb394dfec980f0227edb68cd8f19d0768c0657
leehakseung123 <hak9802022@naver.com>
2025-04-29 14:43:29 +0900

    feat: initial spring-boot project scaffold

---
+++ src/main/java/com/study/jskkiosk/JskKioskApplication.java
@@ -0,0 +1,13 @@
+package com.study.jskkiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JskKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JskKioskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/jskkiosk/JskKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jskkiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JskKioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
